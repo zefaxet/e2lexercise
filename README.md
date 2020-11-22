7f5bd9ab267bf6b9a9a7f2ecc815823a2bef0aa6
EJ <e@j>
2020-11-22 15:53:21 -0600

    H2 + Hibernate DDL up

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # e2lexercise
 Hi there
+
+# Notes
+This uses Lombok, which is a boilerplate helper.
+If you open this project in an IDE without Lombok support, you may see a lot of red. Even so, the project will still compile.
+If you would like to eliminate the red, Eclipse and IntelliJ both have Lombok pluigns, but this is not required.
--- src/main/java/es/auttonberri/exercise/ExerciseApplication.java
+++ src/main/java/es/auttonberri/exercise/ExerciseApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExerciseApplication {
+public class ExerciseApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ExerciseApplication.class, args);
 	}
 
